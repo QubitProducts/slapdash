b30fabb2ecefc56c64f9cb47c974acab6161e1df
Jim O'Brien <jim.obrien@qubit.com>
2016-04-26 13:34:51 +0100

    Add SauceLabs support

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,37 @@
 module.exports = function (config) {
+  var customLaunchers = {
+    sl_ios_safari: {
+      base: 'SauceLabs',
+      browserName: 'iPhone 4S',
+      platform: 'OS X 10.10',
+      version: '8.0'
+    },
+    sl_android_40: {
+      base: 'SauceLabs',
+      browserName: 'Android Emulator',
+      patform: 'Linux',
+      version: '4.0'
+    },
+    sl_ie_8: {
+      base: 'SauceLabs',
+      browserName: 'Internet Explorer',
+      platform: 'Windows XP',
+      version: '8.0'
+    },
+    sl_ie_9: {
+      base: 'SauceLabs',
+      browserName: 'Internet Explorer',
+      platform: 'Windows 7',
+      version: '9.0'
+    },
+    sl_ie_10: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '10.0'
+    }
+  }
+
   config.set({
     frameworks: ['browserify', 'mocha', 'expect', 'sinon'],
 
@@ -24,6 +57,11 @@ module.exports = function (config) {
       noInfo: true
     },
 
-    browsers: ['Chrome', 'Firefox', 'PhantomJS']
+    customLaunchers: customLaunchers,
+    browsers: [
+      'Chrome',
+      'Firefox',
+      'PhantomJS'
+    ]
   })
 }
