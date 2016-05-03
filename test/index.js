2d9bacfcf2fe0ef5b54c196b61f18148f465f5e9
Jim O'Brien <jim.obrien@qubit.com>
2016-05-03 10:01:52 +0100

    Add postinstall step to build; remove index.js

--- test/index.js
+++ test/index.js
@@ -1,17 +1,25 @@
-var slapdash = require('../')
+var slapdash = require('../dist')
+var pkg = require('../package.json')
 
 var methods = ['bind', 'each', 'extend', 'filter', 'find', 'indexOf', 'invoke', 'isMatch', 'keys', 'map', 'matches', 'objectEach', 'objectMap', 'objectReduce', 'pluck', 'reduce', 'slice']
 
-for (var i = 0; i < methods.length; i++) {
-  (function (methodName) {
-    describe(methodName, function () {
-      it('should exist as a property of _', function () {
-        expect(slapdash).to.have.property(methodName)
-      })
+describe('bundle', function () {
+  it('should have name and version', function () {
+    expect(slapdash).to.have.property('name', pkg.name)
+    expect(slapdash).to.have.property('version', pkg.version)
+  })
 
-      it('should be a function', function () {
-        expect(slapdash[methodName]).to.be.a('function')
+  for (var i = 0; i < methods.length; i++) {
+    (function (methodName) {
+      describe(methodName, function () {
+        it('should exist as a property of _', function () {
+          expect(slapdash).to.have.property(methodName)
+        })
+
+        it('should be a function', function () {
+          expect(slapdash[methodName]).to.be.a('function')
+        })
       })
-    })
-  }(methods[i]))
-}
+    }(methods[i]))
+  }
+})
