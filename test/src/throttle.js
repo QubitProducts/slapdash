9458f3500c5138478e4eb920f6816a411cc7f788
Kayle Knopp <kayle@qubit.com>
2020-06-07 19:58:30 +0100

    Add tests

--- test/src/throttle.js
+++ test/src/throttle.js
@@ -41,7 +41,6 @@ module.exports = function (throttle) {
       throttled()
       sinon.assert.calledOnce(tothr)
 
-
       clock.tick(100)
       throttled()
       sinon.assert.calledTwice(tothr)
@@ -72,30 +71,51 @@ module.exports = function (throttle) {
       }, time)
     })
 
-    it('will preserve context', function (done) {
-      var result = null
-      var tothr = function () {
-        result = this
-      }
-      var thr = throttle(tothr, 1)
-      var context = {
-        thr: thr
-      }
-      context.thr()
-      setTimeout(function () {
-        expect(result).to.equal(context)
-        done()
-      }, 1)
+    it('will preserve context', function () {
+      var clock = sinon.useFakeTimers(new Date().getTime())
+      var tothr = sinon.spy()
+      var thr = throttle(tothr, 1, true)
+      var spyCall
+
+      thr()
+      thr()
+      spyCall = tothr.getCall(0)
+      expect(spyCall.calledOn(spyCall.thisValue)).to.equal(true)
+
+      clock.tick(100)
+      thr()
+      thr()
+      spyCall = tothr.getCall(1)
+      expect(spyCall.calledOn(spyCall.thisValue)).to.equal(true)
+      
+      clock.restore()
     })
 
     it('will preserve arguments', function (done) {
       var tothr = sinon.spy()
       var thr = throttle(tothr, 1)
-      thr('fred')
+      thr('fred', [1, 2, 3], { name: 'foo', type: 'bar'})
       setTimeout(function () {
-        expect(tothr.withArgs('fred').calledOnce).to.equal(true)
+        expect(tothr.withArgs('fred', [1, 2, 3], { name: 'foo', type: 'bar'}).calledOnce).to.equal(true)
         done()
       }, 1)
+    })
+
+    it('will return values', function () {
+      var clock = sinon.useFakeTimers(new Date().getTime())
+      var tothr = function(num) { return num * num }
+      var thr = throttle(tothr, 1, true)
+      var returnValue
+      
+      returnValue = thr(3)
+      expect(returnValue).to.equal(9)
+
+      clock.tick(2)
+
+      returnValue = thr(5)
+      expect(returnValue).to.equal(25)
+
+      clock.restore()
     })
   })
 }
